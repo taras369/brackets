9ce2b4a842183bf20cfbddc524c9867239342cac
Taras <taras369ky@gmail.com>
2021-02-16 21:07:13 +0200

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    if (!str || !bracketsConfig || bracketsConfig.length < 1 || str.length < 2 || str.length % 2) {
+         return false;}
+        
+    const array = str.split("");
+    const newA = [];
+
+    for (let e = 0; e < array.length; e++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (array[e] === bracketsConfig[i][1] && newA[newA.length - 1] === bracketsConfig[i][0]){
+                newA.pop();
+            }
+            else if (array[e] === bracketsConfig[i][0]) {
+                newA.push(array[e]);
+            }
+        }
+
+    }
+
+    if (newA.length > 0) {
+        return false;
+    }
+    return true;
+};
